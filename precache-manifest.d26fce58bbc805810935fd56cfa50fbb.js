ddd6819239542a0a544f2ae4525b0368f2c1bc55
vigneshariharan <vigneshsai2211@gmail.com>
2020-07-09 12:22:34 +0530

    first commit

---
+++ precache-manifest.d26fce58bbc805810935fd56cfa50fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9203bfc64de1c964357df96b5a22cca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65fa0af6ea772c1eecf",
+    "url": "/static/css/main.b28d2842.chunk.css"
+  },
+  {
+    "revision": "dd84f3db4ac407200b07",
+    "url": "/static/js/2.50364907.chunk.js"
+  },
+  {
+    "revision": "b65fa0af6ea772c1eecf",
+    "url": "/static/js/main.2621e0fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
